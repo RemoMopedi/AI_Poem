fde517bed78989431cfff7148a0ef9e5d7c6c656
RemoMopedi <135506218+RemoMopedi@users.noreply.github.com>
2024-10-27 16:30:04 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,7 +1,7 @@
-function displayPoem(respose) {
+function displayPoem(response) {
   console.log("poem generated");
   new Typewriter("#poem", {
-    strings: respose.data.answer,
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
@@ -10,12 +10,14 @@ function displayPoem(respose) {
 
 function generatePoem(event) {
   event.preventDefault();
+
   let instructionsInput = document.querySelector("#user-instructions");
 
   let apiKey = "1fcdtd72630b3a012699b80444o4cf56";
-  let prompt = "Generate a poem about ${instructions.Input.value}";
+  let prompt =
+    "User Instructions: Generate a poem about ${instructions.Input.value}";
   let context =
-    "User Instructions: You are a poetry fantic and you wnat to explore more poetry.Your mission is to wite a 4 line poem in basic HTML. Make sure to follow these user instructions.";
+    "You are a poetry fantic and you wnat to explore more poetry.Your mission is to wite a 4 line poem in basic HTML. Make sure to follow these user instructions.";
   let apiUrl =
     "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}";
 
@@ -24,8 +26,6 @@ function generatePoem(event) {
   console.log("Context: ${context}");
 
   axios.get(apiUrl).then(dispayPoem);
-
-  let poemElement = document.querySelector("#poem");
 }
 
 let poemFormElement = document.querySelector("#poem - generator - form");

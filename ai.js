6659fa9ad1b90006df3830a9c055384c0146cfa8
RemoMopedi <135506218+RemoMopedi@users.noreply.github.com>
2024-10-27 16:11:56 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,12 +1,29 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(respose) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: ["Come, And Be My Baby"],
+    strings: respose.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "1fcdtd72630b3a012699b80444o4cf56";
+  let prompt = "Generate a poem about ${instructions.Input.value}";
+  let context =
+    "User Instructions: You are a poetry fantic and you wnat to explore more poetry.Your mission is to wite a 4 line poem in basic HTML. Make sure to follow these user instructions.";
+  let apiUrl =
+    "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}";
+
+  console.log("generating poem");
+  console.log("Prompt: ${prompt}");
+  console.log("Context: ${context}");
+
+  axios.get(apiUrl).then(dispayPoem);
 
   let poemElement = document.querySelector("#poem");
 }

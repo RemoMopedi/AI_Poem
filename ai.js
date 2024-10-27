91402ce2dea916c5c4249d683c8f028fdaac21fc
RemoMopedi <135506218+RemoMopedi@users.noreply.github.com>
2024-10-27 15:04:22 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,1 +1,8 @@
+function generatePoem(event) {
+  event.preventDefault();
+  alert("Generting poem");
+}
+
+let poemFormElement = document.querySelector("#poem - generator - form");
+poemFormElement.addEventListener("submit", generatePoem);
 
